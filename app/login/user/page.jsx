ceb41899ac143d5a632f07e8524bfce72e43f258
Vrushabh D B <51850418+vrushabd@users.noreply.github.com>
2025-11-02 16:48:54 +0530

    Update page.jsx

--- app/login/user/page.jsx
+++ app/login/user/page.jsx
@@ -127,7 +127,7 @@ export default function UserLogin() {
             </div>
             <div className="mt-6 pt-4 border-t border-border text-center">
               <p style={{ marginTop: "15px", color: "white", textAlign: "center" }}>
-          Created by <span style={{ color: "limegreen", fontWeight: "bold" }}>CivicX</span>
+          Created by <span style={{ color: "limegreen", fontWeight: "bold" }}>CivicCare</span>
         </p>
             </div>
           </CardContent>
