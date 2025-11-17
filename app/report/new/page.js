2d44b0e84ba5fa63f5c621d3dbe13fa6ab281a62
Vrushabh D B <51850418+vrushabd@users.noreply.github.com>
2025-11-18 00:12:15 +0530

    Add new report page component

---
+++ app/report/new/page.js
@@ -0,0 +1,9 @@
+// app/report/new/page.js
+export const dynamic = "force-dynamic";
+
+import React from "react";
+import NewReport from "./NewReport"; // if your file name/path differs, adjust accordingly
+
+export default function Page() {
+  return <NewReport />;
+}
